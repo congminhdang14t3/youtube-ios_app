ca85166d0682680d664114b6fe819e3e15a1b267
Minh Đặng Công <minh.dang@codecomplete.jp>
2020-08-18 17:24:00 +0700

    initial Xcode project creation

---
+++ youtube-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-ios-app
+//
+//  Created by Dang Cong Minh on 8/18/20.
+//  Copyright © 2020 Dang Cong Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
